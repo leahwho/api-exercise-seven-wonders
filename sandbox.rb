41b5c040356ba0b84edcbf3631457e1c9da1f069
Leah Hughes <leahwhughes@gmail.com>
2020-03-10 05:39:33 -0700

    bad git hygiene, but want to check .env is not uploaded

---
+++ sandbox.rb
@@ -0,0 +1,45 @@
+response['searchresults']
+
+response = {"searchresults"=>
+  {"place"=>
+    [
+      {"place_id"=>"108563866",
+      "osm_type"=>"way",
+      "osm_id"=>"92425307",
+      "place_rank"=>"30",
+      "boundingbox"=>"32.2817066,32.2820851,35.8906686,35.8912541",
+      "lat"=>"32.2818952",
+      "lon"=>"35.8908989553238",
+      "display_name"=>"The Temple of Artemis, Scaenae frons, Deir ell Leyat, Jarash, Jordan",
+      "class"=>"historic",
+      "type"=>"ruins",
+      "importance"=>"0.46265143530573",
+      "icon"=>"https://locationiq.org/static/images/mapicons/tourist_ruin.p.20.png"},
+      
+      {"place_id"=>"205784542",
+        "osm_type"=>"way",
+        "osm_id"=>"560511793",
+        "place_rank"=>"30",
+        "boundingbox"=>"37.4006812,37.4008183,25.2662148,25.2664119",
+        "lat"=>"37.40074975",
+        "lon"=>"25.2663133407744",
+        "display_name"=>
+        "Temple of Artemis, Sacred Road, Agios Ioannis Diakoftis, Municipality of Mykonos, Mykonos Regional Unit, South Aegean Region, Aegean, 84600, Greece",
+        "class"=>"historic",
+        "type"=>"ruins",
+        "importance"=>"0.301",
+        "icon"=>"https://locationiq.org/static/images/mapicons/tourist_ruin.p.20.png"},
+        
+        {"place_id"=>"146697260",
+          "osm_type"=>"way",
+          "osm_id"=>"244420322",
+          "place_rank"=>"30",
+          "boundingbox"=>"37.6219278,37.6221595,26.0595614,26.059883",
+          "lat"=>"37.6220436",
+          "lon"=>"26.0597222063747",
+          "display_name"=>"Temple Of Artemis, Χ592, Nanouras, Kato Raches, Δήμος Ικαρίας, Regional Unit of Ikaria, North Aegean Region, Aegean, 83301, Greece",
+          "class"=>"historic",
+          "type"=>"ruins",
+        }
+      ]
+    }
--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,26 +1,66 @@
 require 'httparty'
+require 'dotenv'
 
-BASE_URL = "THE BASE URL FOR THE API REQUEST"
-LOCATION_IQ_KEY = "YOUR API TOKEN"
+Dotenv.load
 
-def get_location(search_term)
+unless ENV["LOCATION_IQ_KEY"]
+  puts "Could not load API key; please store in environment variable 'LOCATION_IQ_KEY'."
+  exit
+end
 
+BASE_URL = "https://us1.locationiq.com/v1/search.php"
+LOCATION_IQ_KEY = ENV["LOCATION_IQ_KEY"]
+
+
+def get_location(search_term)   
+  url = BASE_URL
+  
+  query = {
+    "key" => LOCATION_IQ_KEY,
+    "q" => search_term
+  }
+  
+  response = HTTParty.get(url, query: query)
+  location = {}
+  
+  if response['searchresults']['place'].class == Array
+    location = {
+      search_term => {
+        :lat => response["searchresults"]["place"][0]["lat"], :lon=>response["searchresults"]["place"][0]["lon"]
+      }
+    }
+  else location = {
+    search_term => {
+      :lat => response["searchresults"]["place"]["lat"], :lon=>response["searchresults"]["place"]["lon"]
+    }
+  } 
+end
+
+return location
 end
 
 def find_seven_wonders
-
   seven_wonders = ["Great Pyramid of Giza", "Gardens of Babylon", "Colossus of Rhodes", "Pharos of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
-
+  
   seven_wonders_locations = []
-
+  
   seven_wonders.each do |wonder|
     sleep(0.5)
     seven_wonders_locations << get_location(wonder)
   end
-
+  
   return seven_wonders_locations
 end
 
-puts "#{find_seven_wonders}"
+the_wonders = find_seven_wonders
+
+the_wonders.each do |wonder|
+  wonder.map { |place, location| puts "The #{place} is located here: \n#{location}." }
+end
+
+
+# puts "#{find_seven_wonders}"
+
+
 # Expecting something like:
 # [{"Great Pyramid of Giza"=>{:lat=>"29.9791264", :lon=>"31.1342383751015"}}, {"Gardens of Babylon"=>{:lat=>"50.8241215", :lon=>"-0.1506162"}}, {"Colossus of Rhodes"=>{:lat=>"36.3397076", :lon=>"28.2003164"}}, {"Pharos of Alexandria"=>{:lat=>"30.94795585", :lon=>"29.5235626430011"}}, {"Statue of Zeus at Olympia"=>{:lat=>"37.6379088", :lon=>"21.6300063"}}, {"Temple of Artemis"=>{:lat=>"32.2818952", :lon=>"35.8908989553238"}}, {"Mausoleum at Halicarnassus"=>{:lat=>"37.03788265", :lon=>"27.4241455276707"}}]
